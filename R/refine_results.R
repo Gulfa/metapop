5e146da86cd13d440faedbd6f108b2b5dcb39cee
gulfa <gunnarroe@gmail.com>
2022-10-25 21:34:55 +0200

    updated refine results for 2 strains

--- R/refine_results.R
+++ R/refine_results.R
@@ -14,13 +14,32 @@ add_per_n <- function(res, key, params){
 }
 
 
-add_per_age <- function(res, key, params){
+add_per_strain <- function(res, key, params){
+
+  n_list <- seq(1, params$n*params$n_vac,by=1)
+  for(i in 0:(params$n_strain-1)){
+    vals <- res[, paste0(key, "[",n_list+i*params$n*params$n_vac,"]")]
+    if(!is.null(dim(vals))){
+      vals <- rowSums(vals)
+    }
+    res <- cbind(res, vals)
+    colnames(res)[ncol(res)] <- paste0(key, "_strain_", i+1)
+  }
+  return(res)
+}
+
+
+add_per_age <- function(res, key, params, use_strain=TRUE){
 
   fact <- 1
   if(!is.null(params$merge_half_age)){
     fact <- 2
   }
-  n_list <- seq(1, params$n*params$n_vac*params$n_strain,by=params$age_groups/fact)
+  if(use_strain){
+    n_list <- seq(1, params$n*params$n_vac*params$n_strain,by=params$age_groups/fact)
+  }else{
+    n_list <- seq(1, params$n*params$n_vac,by=params$age_groups/fact)
+  }
   for(i in 0:(params$age_groups/fact-1)){
     vals <- res[, paste0(key, "[",n_list+i,"]")]
     if(!is.null(dim(vals))){
@@ -90,11 +109,15 @@ add_per_region <- function(res, key, params){
 
 
 
-add_per_vac <- function(res, key, params){
+add_per_vac <- function(res, key, params, use_strain=TRUE){
   for(i in 1:params$n_vac){
     n_list <- c()
-    for(k in 1:params$n_strain){
-      n_list <- c(n_list, 1:params$n  + (i-1)*params$n + (k-1)*params$n*params$n_vac)
+    if(use_strain){
+      for(k in 1:params$n_strain){
+        n_list <- c(n_list, 1:params$n  + (i-1)*params$n + (k-1)*params$n*params$n_vac)
+      }
+    }else{
+      n_list <- c(n_list, 1:params$n  + (i-1)*params$n)
     }
     vals <- res[, paste0(key, "[",n_list,"]")]
     if(!is.null(dim(vals))){
@@ -128,13 +151,13 @@ refine_results_odin <- function(res, params){
   res <- add_per_age(res, "tot_resp", params)
   res <- add_per_age(res, "tot_hosp", params)
   res <- add_per_age(res, "D", params)
-  res <- add_per_age(res, "tot_vac", params)
-  res <- add_per_age(res, "S", params)
+  res <- add_per_age(res, "tot_vac", params, use_strain=FALSE)
+  res <- add_per_age(res, "S", params, use_strain=FALSE)
+
 
   if(params$n_vac >= 2){
-    res <- add_per_vac(res, "tot_vac", params)
+    res <- add_per_vac(res, "tot_vac", params, use_strain=FALSE)
   }
-  
   if(!is.null(params$N_regions) & params$N_regions > 1){
     res <- add_per_region(res, "tot_infected", params)
     res <- add_per_region(res, "tot_resp", params)
@@ -142,7 +165,7 @@ refine_results_odin <- function(res, params){
     res <- add_per_region(res, "D", params)
   }
   if(params$n_vac > 1){
-    res <- add_per_vac(res, "N", params)
+    res <- add_per_vac(res, "N", params, use_strain = FALSE)
     res <- add_per_vac(res, "tot_infected", params)
     res <- add_per_vac(res, "tot_hosp", params)
   }
@@ -155,13 +178,19 @@ refine_results_odin <- function(res, params){
     res <- add_per_vaccine_prio_group(res, "tot_vac", params, only_reg=TRUE)
 
   }
-  
-#  res <- cbind(res, ICU_R=rowSums(res[, paste0("ICU_R[",1:n,"]")]))
-#  res <- cbind(res, MISC=rowSums(res[, paste0("MISC[",1:n,"]")] +
-#                                 res[, paste0("MISC_ICU_H[",1:n,"]")] 
- #                                ))
- # res <- cbind(res, MISC_ICU=rowSums(res[, paste0("MISC_ICU[",1:n,"]")]))
-  
+  if(params$n_strain > 1){
+    res <- add_per_strain(res, "tot_hosp", params)
+    res <- add_per_strain(res, "tot_infected", params)
+    for(i in 1:params$n_strain){
+      incidence <- c(res[2:N, paste0("tot_infected_strain_",i)] - res[1:(N-1), paste0("tot_infected_strain_",i)],0)
+      res <- cbind(res, incidence)
+      colnames(res)[ncol(res)] <-paste0("incidence_strain_",i)
+      hosp_incidence <- c(res[2:N, paste0("tot_hosp_strain_",i)] - res[1:(N-1), paste0("tot_hosp_strain_",i)],0)
+      res <- cbind(res, hosp_incidence)
+      colnames(res)[ncol(res)] <-paste0("hosp_incidence_strain_",i)
+    }
+  }
+ 
   res <- cbind(res, D=rowSums(res[, paste0("D[",1:n,"]")]))
 
   res <- cbind(res, tot_hosp=rowSums(res[, paste0("tot_hosp[",1:n,"]")]))
@@ -169,73 +198,9 @@ refine_results_odin <- function(res, params){
   res <- cbind(res, tot_prev=rowSums(res[, c(paste0("I[",1:n,"]"), paste0("A[",1:n,"]"), paste0("P[",1:n,"]"))]))
   res <- cbind(res, tot_resp=rowSums(res[, paste0("tot_resp[",1:n,"]")]))
 
-  #vac_numbers <- rowSums(res[, paste0("tot_vac[",vac_list,"]")])
-  #before_start <- vac_numbers[n_vac_start]
-  #vac_from_model <- c(vac_numbers[(params$N_vac_start+1):N], rep(vac_numbers[N], params$N_vac_start)) - before_start
-
-
-  #vac_numbers <- res[, paste0("tot_vac[",vac_list,"]")]
-  #before_start <- vac_numbers[params$N_vac_start,]
-
- ##  tot_vaccinated_all_time_i <- res[, paste0("S[",vac_list_dose1,"]")] +
- ##    res[, paste0("Ea[",vac_list_dose1,"]")] +
- ##    res[, paste0("Es[",vac_list_dose1,"]")] +
- ##    res[, paste0("I[",vac_list_dose1,"]")] +
- ##    res[, paste0("A[",vac_list_dose1,"]")] +
- ##    res[, paste0("P[",vac_list_dose1,"]")] +
- ##    res[, paste0("H[",vac_list_dose1,"]")] +
- ##    res[, paste0("ICU_H[",vac_list_dose1,"]")] +
- ##    res[, paste0("ICU_R[",vac_list_dose1,"]")] +
- ##    res[, paste0("ICU_P[",vac_list_dose1,"]")] +
- ##    res[, paste0("B_D[",vac_list_dose1,"]")] +
- ##    res[, paste0("B_D_H[",vac_list_dose1,"]")] +
- ##    res[, paste0("B_D_ICU[",vac_list_dose1,"]")] +
- ##    res[, paste0("PRE_MISC[",vac_list_dose1,"]")] +
- ##    res[, paste0("MISC[",vac_list_dose1,"]")] +
- ##    res[, paste0("D[",vac_list_dose1,"]")] +
- ##    res[, paste0("R[",vac_list_dose1,"]")] +
- ##    res[, paste0("S[",vac_list_dose2,"]")] +
- ##    res[, paste0("Ea[",vac_list_dose2,"]")] +
- ##    res[, paste0("Es[",vac_list_dose2,"]")] +
- ##    res[, paste0("I[",vac_list_dose2,"]")] +
- ##    res[, paste0("A[",vac_list_dose2,"]")] +
- ##    res[, paste0("P[",vac_list_dose2,"]")] +
- ##    res[, paste0("H[",vac_list_dose2,"]")] +
- ##    res[, paste0("ICU_H[",vac_list_dose2,"]")] +
- ##    res[, paste0("ICU_R[",vac_list_dose2,"]")] +
- ##    res[, paste0("ICU_P[",vac_list_dose2,"]")] +
- ##    res[, paste0("B_D[",vac_list_dose2,"]")] +
- ##    res[, paste0("B_D_H[",vac_list_dose2,"]")] +
- ##    res[, paste0("B_D_ICU[",vac_list_dose2,"]")] +
- ##    res[, paste0("PRE_MISC[",vac_list_dose2,"]")] +
- ##    res[, paste0("MISC[",vac_list_dose2,"]")] +
- ##    res[, paste0("D[",vac_list_dose2,"]")] +
- ##    res[, paste0("R[",vac_list_dose2,"]")] 
-    
- ## # vac_from_model <- rbind(vac_numbers[(params$N_vac_start+1):N,], matrix(rep(vac_numbers[N,], each= params$N_vac_start), ncol=length(vac_list)))
-
- ##  #res <- cbind(res, vac_from_model)
- ##  res <- cbind(res, tot_vac1=rowSums(res[, paste0("tot_vac[",vac_list,"]")]))
- ##  res <- cbind(res, tot_vac_all_time=rowSums(tot_vaccinated_all_time_i))
- ##                                        #  res <- cbind(res, tot_vac2=rep(0, N))
- ##                                        #  res <- cbind(res, tot_vac3=rep(0, N))
-
- ##  res <- cbind(res, tot_vac_all_time_1=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]]))
- ##  res <- cbind(res, tot_vac_all_time_2=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+1]))
- ##  res <- cbind(res, tot_vac_all_time_3=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+2]))
- ##  res <- cbind(res, tot_vac_all_time_4=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+3]))
- ##  res <- cbind(res, tot_vac_all_time_5=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+4]))
- ##  res <- cbind(res, tot_vac_all_time_6=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+5]))
- ##  res <- cbind(res, tot_vac_all_time_7=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+6]))
- ##  res <- cbind(res, tot_vac_all_time_8=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+7]))
- ##  res <- cbind(res, tot_vac_all_time_9=rowSums(tot_vaccinated_all_time_i[,age_list[1:(N_reg*2)]+8]))
-                                                                   
 
   res <- cbind(res, "I"=rowSums(res[, c(paste0("I[",1:n,"]"), paste0("A[",1:n,"]"))]))
   res <- cbind(res, "A"=rowSums(res[, paste0("A[",1:n,"]")]))
-#  res <- cbind(res, "I_vac"=rowSums(res[, c(paste0("I[",vac_list,"]"), paste0("A[",vac_list,"]"))]))
-#  res <- cbind(res, hosp_vac=rowSums(res[, c(paste0("H[",vac_list,"]"), paste0("ICU_P[",vac_list,"]"), paste0("ICU_H[",vac_list,"]"),  paste0("ICU_R[",vac_list,"]"))]))
-  
   res <- cbind(res, ward=rowSums(res[, c(paste0("H[",1:n,"]"), paste0("ICU_P[",1:n,"]"), paste0("ICU_H[",1:n,"]"))]))
   res <- cbind(res, hosp=rowSums(res[, c("ward", paste0("ICU_R[",1:n,"]"))]))
   res <- cbind(res, resp=rowSums(res[, paste0("ICU_R[",1:n,"]")]))
